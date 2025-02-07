501276b5e32ecc7f01515831dd0e23e044345ac5
ruzicjakov <119959707+ruzicjakov@users.noreply.github.com>
2025-02-07 12:28:59 +0100

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Signup from '@/views/SignupView.vue'; 
+import Login from '@/views/LoginView.vue'; 
+import Home from '@/views/HomePage.vue'; 
+import About from '@/views/AboutView.vue'; 
+
+const routes = [
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
