a38be9f144cd5b2286b0ab1bf3a563493c348736
ruzicjakov <119959707+ruzicjakov@users.noreply.github.com>
2025-02-13 00:39:59 +0100

    Update App.vue
    pracenje korisnika

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,27 @@
 
 <script>
 import NavbarButton from "@/components/NavbarButton.vue";
+import { auth } from "@/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    NavbarButton
-  }
+    NavbarButton,
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    onAuthStateChanged(auth, (user) => {
+      this.user = user; // Sprema trenutno prijavljenog korisnika
+    });
+  },
 };
 </script>
+
 
 <style>
 #app {
