34787bec4c312ae98242b174caec80477d61bb1e
ruzicjakov <119959707+ruzicjakov@users.noreply.github.com>
2025-02-13 00:39:15 +0100

    Update index.js
    auth

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase SDK components
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBGvWycN3B8Ab4KnXTLxAjum--t89Mxi9o",
   authDomain: "dog-kennel-13896.firebaseapp.com",
@@ -15,9 +13,9 @@ const firebaseConfig = {
   appId: "1:1001427345023:web:cf7930056bf34056a4e94a"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app); 
 
-export {db};
+export { db, auth };
